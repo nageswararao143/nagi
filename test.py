1d4feb5be082f6fd8124074e76a5644458415a07
nageswararao143 <94102795+nageswararao143@users.noreply.github.com>
2022-05-05 12:49:39 +0530

    test6

--- test.py
+++ test.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
 first test
 2nd test in  feture branch
-<<<<<<< HEAD
-4th test
-=======
 3rd test
->>>>>>> master
-=======
-
-3rd test
-
->>>>>>> 702d42d4fcade4e09bfee9e7fd08c201c69fad3d
